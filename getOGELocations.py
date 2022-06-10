56e47fd1a4b4409b4a3bcfb35a9797201fa4e3af
mchomicky <77936777+mchomicky@users.noreply.github.com>
2022-06-10 11:44:03 -0500

    Update getOGELocations.py
    Redact connection params

--- getOGELocations.py
+++ getOGELocations.py
@@ -1,14 +1,14 @@
 import requests, re, json
-res = requests.get(r'http://tnd-conm-app-1.burnsmcd.com:9000/cmapi/rest/v1/locationservice/locations',
+res = requests.get(r'<redacted>',
                    params={'locationSystem.id': '12', 'locationLevel.id': '35'},
-                   auth=('cmapi', 'cmapi'),
+                   auth=('cmapi', '<redacted>'),
                    headers={'Range': 'items=1-1'})
 
 ctLocs = re.sub(r'items 1-1/(\d+)', r'\1', res.headers['Content-Range'])
 
-res = requests.get(r'http://tnd-conm-app-1.burnsmcd.com:9000/cmapi/rest/v1/locationservice/locations',
+res = requests.get(r'<redacted>',
                    params={'locationSystem.id': '12', 'locationLevel.id': '35'},
-                   auth=('cmapi', 'cmapi'),
+                   auth=('cmapi', '<redacted>'),
                    headers={'Range': f'items=1-{ctLocs}'})
 
 locs = json.loads(res.content)
